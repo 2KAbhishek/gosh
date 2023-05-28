e0d00bcc81eb8e667c9cd61d060fe709b030d911
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-28 07:14:50 +0530

    feat: add initial shell implementation

--- main.go
+++ main.go
@@ -1,8 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "os/exec"
+    "strings"
+)
 
-func main() {
-    fmt.Println("Hello, World!")
+func execInput(input string) error {
+    input = strings.TrimSuffix(input, "\n")
+    cmd := exec.Command(input)
+
+    cmd.Stderr = os.Stderr
+    cmd.Stdout = os.Stdout
+
+    return cmd.Run()
 }
 
+func main() {
+    reader := bufio.NewReader(os.Stdin)
+    currentDir, _ := os.Getwd()
+
+    for {
+        fmt.Printf("%s> ", currentDir)
+        cmdString, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err)
+        }
+
+        if err = execInput(cmdString); err != nil {
+            fmt.Fprintln(os.Stderr, err)
+        }
+    }
+}
