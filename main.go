efad03b9145c453bf39976239829a04a71b56d7e
Abhishek Keshri <iam2kabhishek@gmail.com>
2023-05-28 07:22:49 +0530

    feat: add support for args

--- main.go
+++ main.go
@@ -2,27 +2,18 @@ package main
 
 import (
     "bufio"
+    "errors"
     "fmt"
     "os"
     "os/exec"
     "strings"
 )
 
-func execInput(input string) error {
-    input = strings.TrimSuffix(input, "\n")
-    cmd := exec.Command(input)
-
-    cmd.Stderr = os.Stderr
-    cmd.Stdout = os.Stdout
-
-    return cmd.Run()
-}
-
 func main() {
     reader := bufio.NewReader(os.Stdin)
-    currentDir, _ := os.Getwd()
 
     for {
+        currentDir, _ := os.Getwd()
         fmt.Printf("%s> ", currentDir)
         cmdString, err := reader.ReadString('\n')
         if err != nil {
@@ -33,4 +24,25 @@ func main() {
             fmt.Fprintln(os.Stderr, err)
         }
     }
+}
+
+func execInput(input string) error {
+    input = strings.TrimSuffix(input, "\n")
+    args := strings.Split(input, " ")
+
+    switch args[0] {
+    case "cd":
+        if len(args) < 2 {
+            return errors.New("path required")
+        }
+        return os.Chdir(args[1])
+    case "exit":
+        os.Exit(0)
+    }
+
+    cmd := exec.Command(args[0], args[1:]...)
+    cmd.Stderr = os.Stderr
+    cmd.Stdout = os.Stdout
+
+    return cmd.Run()
 }
